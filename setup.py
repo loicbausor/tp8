2f562e4a5d968a67b06d5107be003ec915190cdd
ArnaudAbreu <35848886+ArnaudAbreu@users.noreply.github.com>
2021-03-08 11:31:04 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,1 +1,17 @@
+from setuptools import setup, find_packages
 
+setup(
+    name="customML",
+    version="0.1",
+    description="custom ML package",
+    author="***",
+    author_email="***",
+    packages=find_packages(),
+    zip_safe=False,
+    install_requires=[
+        "pandas",
+        "numpy",
+        "tqdm",
+    ],
+    include_package_data=True,
+)
